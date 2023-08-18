fefd1d197903ce2b3ef57fa1a50e8f70bf8d6445
bronglil <sajidbrong@gmail.com>
2023-08-18 06:37:32 -0700

    starter kit vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <counter-comp/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import  CounterComp from "./components/CounterComp.vue";
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components : {
+    CounterComp, // Use the corrected component name
   }
 }
 </script>
---
+++ src/components/CounterComp.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="wrapper">
+    <div class="container">
+    <button  @click="increment">+</button>
+    <div>{{ count }}</div>
+    <button  @click="decrement">-</button>
+  </div>
+    <div class="publications">
+      <span>{{ publishedBooks }}</span>
+    </div>
+ </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+        count: 0,
+        name: 'john doe',
+        books: [ ]
+    }
+  },
+  methods: {
+     increment(){
+       this.count ++
+      //  await nextTick()
+    },
+    decrement(){
+        this.count --
+    },
+  },
+  mounted() {
+      this.increment();
+      this.books.push("vue3 - lad from homes")
+      console.log("mounted", this.count);
+  },
+  computed: {
+      publishedBooks(){
+        return this.books.length > 0 ? 'Yes' : 'No'
+      }
+  }
+}
+
+</script>
+
+
+<style>
+.container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+
+.container > button {
+  padding: 10px 20px;
+  margin: 0 10px;
+  cursor: pointer;
+}
+
+.publications{
+  margin-top: 20px;
+}
+</style>
